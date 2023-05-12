c9b19c3e781856b2e5feff63c9cda839776e2431
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-05-12 14:42:43 +0530

    // WEB DEVELOPMENT BOOT CAMP members claps ME #include <stdio.h> int main() {     int i,a,b;     scanf("%d%d",&a,&b);     for(i=a+1;i<b;i++)     {         printf("%d %d %d ",i,i*i,i*i*i);     } }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,12 @@
+// WEB DEVELOPMENT BOOT CAMP members claps ME
+#include <stdio.h>
+int main()
+{
+    int i,a,b;
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+        printf("%d %d %d
+",i,i*i,i*i*i);
+    }
+}
