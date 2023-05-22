f67e6b4c78a169459073fceeb0d7a28fe46aaa28
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-05-22 18:09:40 +0530

    #include <stdio.h> int main() { 	int n; 	scanf("%d", &n); 	int arr[n]; 	int i; 	for(i=0; i<n; i++) 	{ 		scanf("%d", &arr[i]); 	}     int found = 0;     for(i=0; i<n; i++)     {         if(arr[i]!=0&&arr[i]!=1)         {             found = 1;             break;         }     }     if(found==1)     {         printf("False");     }     else     {         printf("True");     } }

---
+++ Binary_Array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int arr[n];
+	int i;
+	for(i=0; i<n; i++) 
+	{
+		scanf("%d", &arr[i]); 
+	}
+    int found = 0;
+    for(i=0; i<n; i++)
+    {
+        if(arr[i]!=0&&arr[i]!=1)
+        {
+            found = 1;
+            break;
+        }
+    }
+    if(found==1)
+    {
+        printf("False");
+    }
+    else
+    {
+        printf("True");
+    }
+}
+
+
