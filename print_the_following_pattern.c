a3ef60680db7f84293603ca53823f8ec9219183d
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-05-23 19:24:58 +0530

    #include<stdio.h>
    int main()
    {
        int n;
        scanf("%d",&n);
        for(int i=1;i<=n;i++)
        {
            for(int j=1;j<=i;j++)
            {
                if(j==1||j==i||i==n)
                {
                    printf("*");
                }
                else
                {
                    printf(" ");
                }
            }
            printf("
    ");
        }
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
+
 int main()
 {
-    int n,i,j;
-scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(int j=1;j<=i;j++)
         {
-            printf("%d",j);
+            if(j==1||j==i||i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
     }
 }
+
+
