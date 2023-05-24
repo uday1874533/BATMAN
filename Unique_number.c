cda51e3e6f0eb6d2e5033d3d74b8cf633c6454c0
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-05-24 20:20:18 +0530

    #include<stdio.h>
    int dc(int n)
    {
        int r,k=0;
        while(n>0)
        {
            r=n%10;
            k++;
            n=n/10;
        }
        return k;
    }
    
    int check(int n,int m)
    {
        int k=0,r;
        while(m>0)
        {
            r=m%10;
            if(n==r)
            {
                k++;
            }
            m=m/10;
        }
        if(k==1)
        {
            return 0;
        }
        else
        {
            return 1;
            
        }
    }
    int main()
    {
        int n,r;
        scanf("%d",&n);
        int m=n,k=0;
        while(n>0)
        {
            r=n%10;
            if(check(r,m))
            {
                k++;
                break;
                
            }
            n=n/10;
            
        }
        if(k==0)
        {
            printf("Unique Number");
        }
        else
        {
            printf("Not Unique Number");
        }
    }

--- Unique_number.c
+++ Unique_number.c
@@ -1,59 +1,62 @@
 #include<stdio.h>
+
+int dc(int n)
+{
+    int r,k=0;
+    while(n>0)
+    {
+        r=n%10;
+        k++;
+        n=n/10;
+    }
+    return k;
+}
+
+int check(int n,int m)
+{
+    int k=0,r;
+    while(m>0)
+    {
+        r=m%10;
+        if(n==r)
+        {
+            k++;
+        }
+        m=m/10;
+    }
+    if(k==1)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+        
+    }
+}
 int main()
 {
-      int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,n,r;
-      scanf("%d",&n);
-      while(n!=0)
-      {
-          r=n%10;
-          if(r==0)
-           {
-               a++;
-           }
-           if(r==1)
-           {
-               b++;
-           }
-           if(r==2)
-           {
-               c++;
-           }
-           if(r==3)
-           {
-               d++;
-           }
-           if(r==4)
-           {
-               e++;
-           }
-           if(r==5)
-           {
-               f++;
-           }
-           if(r==6)
-           {
-               g++;
-           }
-           if(r==7)
-           {
-               h++;
-           }
-           if(r==8)
-           {
-               i++;
-           }
-           if(r==9)
-           {
-               j++;
-           }
-           n=n/10;
-      }
-      if(a<2 && b<2 && c<2 && d<2 && e<2 && f<2 && g<2 && h<2 && i<2 && j<2 )
-      {
-          printf("Unique Number");
-      }
-      else
-      {
-          printf("Not Unique Number");
-      }
+    int n,r;
+    scanf("%d",&n);
+    int m=n,k=0;
+    while(n>0)
+    {
+        r=n%10;
+        if(check(r,m))
+        {
+            k++;
+            break;
+            
+        }
+        n=n/10;
+        
+    }
+    if(k==0)
+    {
+        printf("Unique Number");
+    }
+    else
+    {
+        printf("Not Unique Number");
+    }
 }
