d9218387fd31e863aa1402d7396ae7e35926a49e
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-05-25 09:41:38 +0530

    #include <stdio.h> int main() { 	int n; 	scanf("%d", &n); 	int arr[n]; 	int i,sum=0; 	for(i=0; i<n; i++) 	{ 		scanf("%d", &arr[i]); 	} 	for(i=0;i<n;i++) 	{ 		sum+=arr[i]; 	}
    	printf("%.2f",(float)sum/n);
    }

---
+++ Average_of_the_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int arr[n];
+	int i,sum=0;
+	for(i=0; i<n; i++) 
+	{
+		scanf("%d", &arr[i]); 
+	}
+	for(i=0;i<n;i++)
+	{
+		sum+=arr[i];
+	}
+	
+	printf("%.2f",(float)sum/n);
+}
