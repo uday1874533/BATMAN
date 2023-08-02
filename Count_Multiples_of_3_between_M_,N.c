bea6dd62c2c747571baf571383e1665581e5792c
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-02 15:48:10 +0530

    #include<stdio.h> int main() {     int m,n,k=0;     scanf("%d%d",&m,&n);     for (int i=m;i<=n;i++)     {         if(i%3==0)         {             k++;         }     }     printf("%d",k); }

---
+++ Count_Multiples_of_3_between_M_,N.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,k=0;
+    scanf("%d%d",&m,&n);
+    for (int i=m;i<=n;i++)
+    {
+        if(i%3==0)
+        {
+            k++;
+        }
+    }
+    printf("%d",k);
+}
