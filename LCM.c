7f8d73eed90290c78b5e418532da53fac85f308d
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-03 18:26:51 +0530

    #include <stdio.h> int main() {     int i=0,n,m;     scanf("%d%d",&n,&m);     while(1)     {         i++;         if(n*i%m==0)         {             break;         }     }     printf("%d",n*i); }

--- LCM.c
+++ LCM.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-int main ()
-{
-	int i=0,n,m;
-	scanf("%d %d",&n,&m);
-	while(1)
-	{
-	    i++;
-	    if(n*i%m==0)
-		{
-			break;
-		}
-	}
-	printf("%d ",n*i);
-}
 
+#include <stdio.h>
+int main()
+{
+    int i=0,n,m;
+    scanf("%d%d",&n,&m);
+    while(1)
+    {
+        i++;
+        if(n*i%m==0)
+        {
+            break;
+        }
+    }
+    printf("%d",n*i);
+}
