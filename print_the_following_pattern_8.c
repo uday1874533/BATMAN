bdf8fbb6ccad82f8f90efc332990c0f45d8cf834
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-04 10:25:35 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=n-2;j++)         {             printf("%d",j);         }         int k=n-3;         for (int j=k;j>=1;j--)         {             printf("%d",j);         }         printf(" ");     } }

---
+++ print_the_following_pattern_8.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-2;j++)
+        {
+            printf("%d",j);
+        }
+        int k=n-3;
+        for (int j=k;j>=1;j--)
+        {
+            printf("%d",j);
+        }
+        printf("
+");
+    }
+}
