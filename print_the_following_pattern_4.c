5d5ca5d74f5100f3d64a0250f5113aeb3466ffb0
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-04 10:15:43 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=n;j++)         {             if(j==i||j+i==n+1)             {                 printf("x");             }             else             {                 printf("0");             }         }         printf(" ");     } }

---
+++ print_the_following_pattern_4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j==i||j+i==n+1)
+            {
+                printf("x");
+            }
+            else
+            {
+                printf("0");
+            }
+        }
+        printf("
+");
+    }
+}
