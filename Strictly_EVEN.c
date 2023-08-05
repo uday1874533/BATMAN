c37f99e1f1cc32230df312422096abd65a9cd8ed
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-05 11:19:42 +0530

    #include<stdio.h> int main() {     int n,t=0,ev=0;     scanf("%d",&n);     int x[n];     for(int i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(int i=0;i<n;i++)     {         if(x[i]%2==0)         {             ev++;         }     }     for(int i=0;i<n;i++)     {         if(x[i]%2==0 &&i%2==0)         {             t++;         }     }     if(t==ev)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_EVEN.c
+++ Strictly_EVEN.c
@@ -1,32 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	int n;
-	scanf("%d", &n);
-	int arr[n];
-	int i;
-	for(i=0; i<n; i++) 
-	{
-		scanf("%d", &arr[i]); 
-	}
-	int found=0;
-	for(i=0; i<n; i++)
-	{
-		if(i%2!=0)
-		{
-			if(arr[i]%2==0)
-			{
-				found = 1;
-                break;
-			}
-		}
-	}
-	if(found==1)
-	{
-		printf("False");
-	}
-	else
-	{
-		printf("True");
-	}
+    int n,t=0,ev=0;
+    scanf("%d",&n);
+    int x[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(x[i]%2==0)
+        {
+            ev++;
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(x[i]%2==0 &&i%2==0)
+        {
+            t++;
+        }
+    }
+    if(t==ev)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
