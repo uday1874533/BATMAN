ecb4363dcd5901c23cd8357f5e048ec054169a9a
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-05 13:01:36 +0530

    #include<stdio.h> int main() {     int m,j;     scanf("%d",&m);     for(j=1;j<=m;j++)     {         int n;         scanf("%d",&n);         int pp=n;         while(1)         {             int fc=0;             int i;             for(i=1;i<=pp;i++)             {                 if(pp%i==0)                 {                     fc++;                 }             }             if(fc==2)             {                 break;             }             pp--;         }         int np=n;         while(1)         {             int fc=0;             int i;             for(i=1;i<=np;i++)             {                 if(np%i==0)fc++;             }             if(fc==2)             {                 break;             }             np++;         }         int res=n-pp<=np-n? pp:np;         printf("%d ",res);     } }

--- Nearest_Prime.c
+++ Nearest_Prime.c
@@ -1,47 +1,47 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-	int m,j;
-	scanf("%d",&m);
-	for(j=1;j<=m;j++)
-	{
-		int n;
-		scanf("%d",&n);
-		int pp=n;
-		while(1)
-		{
-			int fc=0;
-			int i;
-			for(i=1;i<=pp;i++)
-			{
-				if(pp%i==0)
-				{
-					fc++;
-				} 
-			}
-			if(fc==2)
-			{
-				break;
-			}
-			pp--;
-		}
-		int np=n;
-		while(1)
-		{
-			int fc=0;
-			int i;
-			for(i=1;i<=np;i++)
-			{
-				if(np%i==0) fc++;
-			}
-			if(fc==2)
-			{
-				break;
-			}
-			np++;
-		}
-		int res=n-pp<=np-n ? pp:np;
-		printf("%d
+    int m,j;
+    scanf("%d",&m);
+    for(j=1;j<=m;j++)
+    {
+        int n;
+        scanf("%d",&n);
+        int pp=n;
+        while(1)
+        {
+            int fc=0;
+            int i;
+            for(i=1;i<=pp;i++)
+            {
+                if(pp%i==0)
+                {
+                    fc++;
+                }
+            }
+            if(fc==2)
+            {
+                break;
+            }
+            pp--;
+        }
+        int np=n;
+        while(1)
+        {
+            int fc=0;
+            int i;
+            for(i=1;i<=np;i++)
+            {
+                if(np%i==0)fc++;
+            }
+            if(fc==2)
+            {
+                break;
+            }
+            np++;
+        }
+        int res=n-pp<=np-n? pp:np;
+        printf("%d
 ",res);
-	}
+    }
 }
